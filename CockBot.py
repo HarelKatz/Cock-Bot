eab43ac3fc16bd2cb04aa2ba64c44c3e70c7aee8
Harel Katz <harelka2@gmail.com>
2020-05-04 23:39:34 +0300

    Added small documentation

--- CockBot.py
+++ CockBot.py
@@ -1,11 +1,11 @@
-# bot.py
 import os
 import random
 import asyncio
 
-import discord
-from dotenv import load_dotenv
+import discord  # pip install discord
+from dotenv import load_dotenv  # pip install python-dotenv
 
+# loads all of the enviroment variables
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
@@ -15,7 +15,7 @@ MAIN_CHANNEL_ID = int(os.getenv('MAIN_CHANNEL_ID'))
 
 client = discord.Client()
 
-
+# when the bot is started
 @client.event
 async def on_ready():
 
@@ -31,10 +31,11 @@ async def on_ready():
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
-
+# when someone send a message in chat
 @client.event
 async def on_message(message):
 
+    # if the message is the bot's message or he recives a message in the DMs
     if message.author == client.user or message.guild == None:
         return
 
@@ -45,6 +46,7 @@ async def on_message(message):
             main_channel = i
             break
 
+    # if someone says "99!"
     if message.content == '99!':
 
         brooklyn_99_quotes = [
@@ -59,12 +61,14 @@ async def on_message(message):
         await message.channel.send(response)
         sent_message(message.channel.name, response)
 
+    # someone says "my cock"
     elif 'my cock' in message.content.lower():
 
         response = message.author.mention + " Nice cock bro"
         await message.channel.send(response)
         sent_message(message.channel.name, response)
 
+    # if someone say that someone else dick is small
     elif all(word in message.content.lower() for word in ["your", "dick", "small"]):
 
         response = message.author.mention + " dick's is small"
@@ -75,6 +79,7 @@ async def on_message(message):
         await message.channel.send(response)
         sent_message(message.channel.name, response)
 
+    # if someone @ the bot and calls him agy
     elif client.user.mentioned_in(message) and "gay" in message.content.lower():
 
         response = message.author.mention + " lol bye"
@@ -95,11 +100,13 @@ async def on_message(message):
         except:
             pass
 
+    # if someone @ the bot
     elif client.user.mentioned_in(message) and message.content == "<@!" + str(client.user.id) + ">":
         response = message.author.mention + " what"
         await message.channel.send(response)
         sent_message(message.channel.name, response)
 
+    # if someone @ the bot and says: "wow"
     elif client.user.mentioned_in(message) and (message.content.replace(" ", "").replace("<@!" + str(client.user.id) + ">", '') == "wow"):
         response = '''וווואווווווווו😱😱😱😱😱
 אתם לא מאמינים זה אשכרה עובד!!! אם תגידו את המשפט שכתוב פה למטה שלוש פעמים ותשתפו לחמש קבוצות אתם לא תאמינו מה קורה!!!!!!!!!!!!!!!! האייקון של הדיסקורד יהפוך לאדום‼️‼️‼️‼️
@@ -110,7 +117,7 @@ async def on_message(message):
         await message.channel.send(response)
         sent_message(message.channel.name, response)
 
-
+# when someone joins a server
 @client.event
 async def on_member_join(member):
 
