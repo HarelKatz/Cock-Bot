9e6ba0c4cc6cfde5a68a5128d754a16eb7d11eb5
Harel Katz <harelka2@gmail.com>
2020-05-04 22:44:44 +0300

    Added new features added a wow feature, prepare to be amazed added a greeting feature when someone enters the server fixed that only one response will be made when a couple of them are available added a what feature

--- CockBot.py
+++ CockBot.py
@@ -10,6 +10,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 MAIN_CHANNEL_NAME = os.getenv('MAIN_CHANNEL_NAME')
+MAIN_CHANNEL_ID = int(os.getenv('MAIN_CHANNEL_ID'))
 
 
 client = discord.Client()
@@ -24,6 +25,8 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+    for channel in guild.channels:
+        print(channel, channel.id)
 
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
@@ -32,7 +35,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
-    if message.author == client.user:
+    if message.author == client.user or message.guild == None:
         return
 
     guild = discord.utils.get(client.guilds, name=GUILD)
@@ -56,15 +59,15 @@ async def on_message(message):
         await message.channel.send(response)
         sent_message(message.channel.name, response)
 
-    if 'my cock' in message.content.lower():
+    elif 'my cock' in message.content.lower():
 
         response = message.author.mention + " Nice cock bro"
         await message.channel.send(response)
         sent_message(message.channel.name, response)
 
-    if all(word in message.content.lower() for word in ["your", "dick", "small"]):
+    elif all(word in message.content.lower() for word in ["your", "dick", "small"]):
 
-        response = message.author.mention + " dick is small"
+        response = message.author.mention + " dick's is small"
         await message.channel.send(response)
         sent_message(message.channel.name, response)
 
@@ -72,7 +75,7 @@ async def on_message(message):
         await message.channel.send(response)
         sent_message(message.channel.name, response)
 
-    if client.user.mentioned_in(message) and "gay" in message.content.lower():
+    elif client.user.mentioned_in(message) and "gay" in message.content.lower():
 
         response = message.author.mention + " lol bye"
         await message.channel.send(response)
@@ -91,6 +94,36 @@ async def on_message(message):
 
         except:
             pass
+
+    elif client.user.mentioned_in(message) and message.content == "<@!" + str(client.user.id) + ">":
+        response = message.author.mention + " what"
+        await message.channel.send(response)
+        sent_message(message.channel.name, response)
+
+    elif client.user.mentioned_in(message) and (message.content.replace(" ", "").replace("<@!" + str(client.user.id) + ">", '') == "wow"):
+        response = '''וווואווווווווו😱😱😱😱😱
+אתם לא מאמינים זה אשכרה עובד!!! אם תגידו את המשפט שכתוב פה למטה שלוש פעמים ותשתפו לחמש קבוצות אתם לא תאמינו מה קורה!!!!!!!!!!!!!!!! האייקון של הדיסקורד יהפוך לאדום‼️‼️‼️‼️
+
+אַשְהַדֻ אַן לַא אִלָהַ אִלַּא אללָּה וַאַן מֻחַמַּדַ(ן) רַסוּלֻ אללָּה.
+
+כדאי לכם ממממשששש לנסות לי זה עבד וזה ממש מגניבב🤭🤭🤭'''
+        await message.channel.send(response)
+        sent_message(message.channel.name, response)
+
+
+@client.event
+async def on_member_join(member):
+
+    if member == client.user:
+        return
+
+    response = member.mention + \
+        " Nice to see you hear, if you are a girl **GET OUT** if not nice ding dong bro"
+
+    main_channel = client.get_channel(MAIN_CHANNEL_ID)
+    print(main_channel, main_channel.id)
+    await main_channel.send(response)
+    sent_message(main_channel.name, response)
 
 
 def sent_message(channel_name, response):
