9bce04cf162ef505a46a4dc35cde683395dcd97f
Harel Katz <harelka2@gmail.com>
2020-05-04 18:10:56 +0300

    Got rid of dupe code added a function that writes a response to the screen Fixes #2

--- CockBot.py
+++ CockBot.py
@@ -28,21 +28,12 @@ async def on_ready():
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
-    '''for member in guild.members:
-        if member.name == 'Gilbert with no brim':
-            while 1:
-                try:
-                    await member.create_dm()
-                    await member.dm_channel.send("you are stupid")
-                except:
-                    pass'''
-
-    for i in guild.channels:
-        print(i.name, type(i.name))
-
 
 @client.event
 async def on_message(message):
+
+    if message.author == client.user:
+        return
 
     guild = discord.utils.get(client.guilds, name=GUILD)
     main_channel = None
@@ -51,66 +42,59 @@ async def on_message(message):
             main_channel = i
             break
 
-    if message.author == client.user:
-        return
-
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
     if message.content == '99!':
 
+        brooklyn_99_quotes = [
+            'I\'m the human form of the 💯 emoji.',
+            'Bingpot!',
+            (
+                'Cool. Cool cool cool cool cool cool cool, '
+                'no doubt no doubt no doubt no doubt.'
+            ),
+        ]
         response = random.choice(brooklyn_99_quotes)
         await message.channel.send(response)
-        print("Sent message @ " + message.channel.name +
-              ' that says "' + response + '"')
+        sent_message(message.channel.name, response)
 
     if 'my cock' in message.content.lower():
 
         response = message.author.mention + " Nice cock bro"
         await message.channel.send(response)
-        print("Sent message @ " + message.channel.name +
-              ' that says "' + response + '"')
+        sent_message(message.channel.name, response)
 
-    if 'your dick' in message.content.lower():
+    if all(word in message.content.lower() for word in ["your", "dick", "small"]):
 
         response = message.author.mention + " dick is small"
         await message.channel.send(response)
-        print("Sent message @ " + message.channel.name +
-              ' that says "' + response + '"')
-
-        '''og_message = message.content
-        await message.edit("My dick is very small")
-        print("Edited " + message.author + " message from " +
-              og_message + ' to "My dick is very small')'''
+        sent_message(message.channel.name, response)
 
         response = message.author.mention + " Yes, your dick is small"
         await message.channel.send(response)
-        print("Sent message @ " + message.channel.name +
-              ' that says "' + response + '"')
+        sent_message(message.channel.name, response)
 
     if client.user.mentioned_in(message) and "gay" in message.content.lower():
 
         response = message.author.mention + " lol bye"
         await message.channel.send(response)
-        print("Sent message @ " + message.channel.name +
-              ' that says "' + response + '"')
+        sent_message(message.channel.name, response)
         await asyncio.sleep(5)
+
         try:
-            await message.author.kick()
-            print("kicking " + message.author.name + " for the lolz")
+
             await message.author.create_dm()
             await message.author.dm_channel.send("you are stupid")
             await message.author.dm_channel.send("you should be sent a new link to the server, if not just search for another link or ask some one to dend you a link")
             link = await main_channel.create_invite(max_age=300, max_uses=1, reason="kicked him for the lolz")
             await message.author.dm_channel.send("Here is a 1 time use invite link: " + link.url)
+            await message.author.kick()
+            print("kicking " + message.author.name + " for the lolz")
 
         except:
             pass
+
+
+def sent_message(channel_name, response):
+    print("Sent message @ " + channel_name + ' that says "' + response + '"')
+
 
 client.run(TOKEN)
