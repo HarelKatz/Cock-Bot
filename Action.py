f080f0869d2af7c7b51bb02662343ecd5afb6962
Harel Katz <harelka2@gmail.com>
2020-05-05 18:51:48 +0300

    Fixed that the defualt should_respond be the trigger

--- Action.py
+++ Action.py
@@ -3,7 +3,7 @@ class Action:
         self._trigger = trigger.lower()
 
     def should_respond(self, message, client):
-        return self._trigger in message.content.lower()
+        return self._trigger == message.content.lower()
 
     async def do_action(self, message, client, discord):
         return message, True
